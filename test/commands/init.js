60aecc5268298fa3261178a2ed4e051075a60f82
David Silva <contacto@dvidsilva.com>
2016-11-15 16:34:21 -0800

    fixed broken build test, added test to init comand

--- test/commands/init.js
+++ test/commands/init.js
@@ -1,6 +1,20 @@
-var should = require('chai').should();
+var should = require('chai').should;
+var expect = require('chai').expect;
+
 var init = require('../../lib/commands/init');
+var fs = require(`../../lib/utils/fs`);
+
+const name = 'sample';
 
 describe('init command is a valid module', function () {
   it('will always pass unless init is undefined', function() {});
+});
+
+describe('will run git init with a sample folder', function () {
+    it (`will create a new project in the specified folder`, function () {
+        fs.rmdir(name);
+        init.handler({name: name, interactive: false, force: true});
+        let dir = fs.statSync(name);
+        expect(dir.isDirectory()).to.equal(true);
+    });
 });
