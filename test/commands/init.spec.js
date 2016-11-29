ed59a2469745a5434974857d69f85225c6bd691f
dfrownfelter <davidrfrownfelter@gmail.com>
2016-11-29 14:13:21 -0800

    Add showSiteInfo

--- test/commands/init.spec.js
+++ test/commands/init.spec.js
@@ -1,8 +1,10 @@
 var should = require('chai').should;
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 
 var init = require('../../lib/commands/init');
 var fs = require(`../../lib/utils/fs`);
+var Project = require('../../lib/core/project');
 
 const name = 'sample';
 
@@ -19,5 +21,25 @@ describe('will run git init with a sample folder', function () {
         init.handler({ name: name, interactive: false, force: true });
         let dir = fs.statSync(name);
         expect(dir.isDirectory()).to.equal(true);
+    });
+});
+
+describe('init', function () {
+    describe('optionsBuilder', function () {
+        it('should exist', function () {
+            assert.isFunction(init.optionsBuilder);
+        }
+    );
+        it('should have valid keys', function () {
+            const option = { name: '' };
+            expect(init.optionsBuilder(option)).to
+            .have.all.keys('name', 'location', 'longname', 'layout', 'author');
+        });
+    }
+);
+    describe('showSiteInfo', function () {
+        it('should exist', function () {
+            assert.isFunction(init.showSiteInfo);
+        });
     });
 });
