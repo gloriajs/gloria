e2d26d073c573edca637abb74b224acc9bf62c2c
dfrownfelter <davidrfrownfelter@gmail.com>
2016-12-12 19:37:41 -0800

    Refactor serveAndBuildSite

--- test/commands/serve.spec.js
+++ test/commands/serve.spec.js
@@ -1,8 +1,26 @@
 var should = require('chai').should();
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 
-var build = require('../../lib/commands/serve');
+var serve = require('../../lib/commands/serve');
+var Project = require('../../lib/core/project');
 
 describe('serve command is a valid module', function () {
     it('will always pass unless serve is undefined', () => null);
+});
+
+describe('serve', function () {
+    describe('serveAndBuildSite', function () {
+            it('exists', function () {
+                    assert.isFunction(serve.serveAndBuildSite);
+                }
+            );
+            /*it('returns an Express object', function () {
+                const project = new Project();
+                const argv = {};
+                const options = project.loadConfig('yaml');
+                const dest = project.config.dest || argv.dest;
+                assert.isObject(serve.serveAndBuildSite(argv, dest), 'returns express object');
+            });*/
+        });
 });
